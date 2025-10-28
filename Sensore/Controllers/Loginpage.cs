2a50145b650ec00882d0f9d695dc838323ada939
Qudus-oa <USER@DESKTOP-0IL6RM7>
2025-10-28 09:21:22 +0000

    controller and view for login/create page

---
+++ Sensore/Controllers/Loginpage.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sensore.Controllers
+{
+    public class Loginpage : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+      
+    
+    }
+    
+
+
+}
