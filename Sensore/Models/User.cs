e4d91e945e323405b33c11460fcbabf0f91cbdf6
Qudus-oa <USER@DESKTOP-0IL6RM7>
2025-11-08 23:12:44 +0000

    First entity

---
+++ Sensore/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Sensore.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public UserType Type { get; set; } // Patient, Clinician, Admin
+        public DateTime CreatedDate { get; set; }
+    }
+    public enum UserType
+    {
+        Patient,
+        Clinician,
+        Admin
+    }
+
+}
