affaecf58381d1f55432117986b072f2ed64d7b6
Qudus-oa <USER@DESKTOP-0IL6RM7>
2025-11-16 00:09:15 +0000

    updated model after adding primary key

--- Sensore/Models/User.cs
+++ Sensore/Models/User.cs
@@ -1,18 +1,28 @@
-﻿namespace Sensore.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sensore.Models
 {
     public class User
     {
-        public int UserId { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public UserType Type { get; set; } // Patient, Clinician, Admin
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Password{ get; set; }
+        public UserType Type { get; set; } /* Patient, Clinician, Admin*/
         public DateTime CreatedDate { get; set; }
-    }
-    public enum UserType
-    {
-        Patient,
-        Clinician,
-        Admin
+
     }
 
 }
+public enum UserType
+{
+    Admin,
+    Clinician,
+    patient
+}
+
