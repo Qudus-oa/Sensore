173d496027f7f66ea764e4392840c4a19536b03f
Qudus-oa <USER@DESKTOP-0IL6RM7>
2025-12-09 12:55:21 +0000

    users model

--- Sensore/Models/User.cs
+++ Sensore/Models/User.cs
@@ -5,20 +5,16 @@ namespace Sensore.Models
 {
     public class User
     {
-
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
         public required string Email { get; set; }
         public required string Password{ get; set; }
         public UserType Type { get; set; } /* Patient, Clinician, Admin*/
         public DateTime CreatedDate { get; set; }
-
     }
-
 }
+
 public enum UserType
 {
     Admin,
