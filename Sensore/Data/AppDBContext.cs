cc6500af68c108edf05a75a43572f0b7ab6a6ba0
Qudus-oa <USER@DESKTOP-0IL6RM7>
2025-12-15 04:52:09 +0000

    updated Appdb context

---
+++ Sensore/Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Sensore.Data
+{
+
+    using Microsoft.EntityFrameworkCore;
+    using Sensore.Models;
+
+    public class AppDBContext:DbContext
+    {
+       public AppDBContext (DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> users { get; set; }
+    }
+}
