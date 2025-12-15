94f37af40a3e845fc107f8c0b85a186d05c7dd8d
Qudus-oa <USER@DESKTOP-0IL6RM7>
2025-12-15 04:47:03 +0000

    ClinicianHomeController

---
+++ Sensore/Controllers/AdminHomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sensore.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        // Admin Dashboard
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Redirect to Manage Users page
+        [HttpGet]
+        public IActionResult GoToManageUsers()
+        {
+            return RedirectToAction("Index", "ManageUsers");
+        }
+    }
+}
